b3ea95ae7264b703c5b62c6329e37312f9c06802
Florent Heyworth <florent@napalu.ch>
2026-04-02 18:02:59 +0200

    update for Developer ID codesign

--- scripts/codesign.sh
+++ scripts/codesign.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# Called by goreleaser signs section. Only signs Mach-O binaries.
 set -euo pipefail
 
-: "${CODESIGN_IDENTITY:=-}"
+: "${CODESIGN_IDENTITY:?CODESIGN_IDENTITY is required}"
 : "${CODESIGN_IDENTIFIER:=tech.heyworth.sympa}"
+
 artifact="$1"
 
 if file "$artifact" | grep -q "Mach-O"; then
-  codesign -s "$CODESIGN_IDENTITY" \
+  codesign --force \
+    --sign "$CODESIGN_IDENTITY" \
     --identifier "$CODESIGN_IDENTIFIER" \
     --options runtime \
     --timestamp \
